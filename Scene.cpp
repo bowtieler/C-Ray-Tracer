8321d81e4b9439288f55c884c57e190bf57e428b
bowtieler <tyleradam@sbcglobal.net>
2015-06-06 17:55:21 -0500

    Update scene.cpp

--- Scene.cpp
+++ Scene.cpp
@@ -6,9 +6,9 @@
 
 void scene::DrawScene(char *outputFileName)
 {
-	int w = win->Width(); //. num of pixels disp on screen
+	int w = win->Width(); // num of pixels disp on screen
 	int h = win->Height();
-	double pixelW = windowW / w; //. in the world
+	double pixelW = windowW / w; //in the world
 	double pixelH = windowH / h;
 	
 
@@ -22,19 +22,18 @@ void scene::DrawScene(char *outputFileName)
 		delete bit;
 	bit = new Bitmap(*win, w, h);
 
-	// from camera to target
-	vector view; 
+	vector view; // from camera to target
 	// = (T-C)/|T-C|, where T = target and C = Camera location
 	
-	vector TmC = target - camera;	//.
-	view = TmC / TmC.Length(); //.
+	vector TmC = target - camera;	
+	view = TmC / TmC.Length();
 	cout << "View = " << view << endl;
 
-	double d = Distance(point(camera[0],camera[1], camera[2]), point(windowD)); //.
+	double d = Distance(point(camera[0],camera[1], camera[2]), point(windowD));
 	cout << "d = " << d << endl;
 
 	// Calculate the center of the window
-	point center = camera + d*view; //.
+	point center = camera + d*view;
 	// = C+d*v, where d = distance to the window and v = vector
 	cout << "Center = " << center << endl;
 
@@ -42,17 +41,17 @@ void scene::DrawScene(char *outputFileName)
 	// so that it is perpendicular to the view vector, and
 	// normalize it.
 
-	up.Perp(view); //.
-	up.Normalize(); //.
+	up.Perp(view);
+	up.Normalize();
 	cout << "Up = " << up << endl;
 
 	// Now calculate the left vector.
-	vector left = Cross(up, view); //.
+	vector left = Cross(up, view);
 	//left += view;
 	cout << "Left = " << left << endl;
 
 	// Calculate the coordinates of upperLeft = pixel(0, 0)
-	point upperLeft = center + up*(windowH/2) + (windowW/2)*left; //.
+	point upperLeft = center + up*(windowH/2) + (windowW/2)*left;
 	cout << "upperLeft = " << upperLeft << endl;
 
 	int t1 = clock();
@@ -62,20 +61,26 @@ void scene::DrawScene(char *outputFileName)
 		for (int i = 0; i < w; i++)
 		{
 			// Calculate the position of pixel(i, j) in space
-			point screenP = upperLeft - (i*pixelW*left) - (j*pixelH*up); //.
-			point ijSpace = screenP; //.
-			ijSpace[0] = 0; //.
+			point screenP = upperLeft - (i*pixelW*left) - (j*pixelH*up);
+			point ijSpace = screenP;
+			ijSpace[0] = 0;
+
 
 			
 			// Then build the ray from the camera to the pixel
 			ray r;
-			r.p = camera; //.
-			r.v = (screenP - camera); //.
-			r.v.Normalize(); //.
-			r.v[0] = 0; //.
+			r.p = camera;
+			r.v = (screenP - camera);
+			r.v.Normalize();
+			if (i==j && c%10==0) {
+				//cout << "@ i/j = " << i << " / " << j << ", screenP = " << screenP << endl;
+				cout << "Ray @ " << r.p << " -> " << r.v << endl;
+			}
+			
 
 			// Finally, we trace the ray to get the color we see,
-			// clamp the color and write the result to the screen
+			// clamp the color void f = ({ r = clamp(r, 0.0, 1.0); g = clamp(g, 0.0, 1.0); b = clamp(b, 0.0, 1.0); })
+			// and write the result to the screen
 			rgb result = TraceRay(r, i, j);
 			result.Clamp();
 			bit->Point(i, j, result.R(), result.G(), result.B());							
@@ -93,14 +98,13 @@ bool scene::CastRay(const ray &r, intersection &inter)
 {
 	bool found = false;
 	intersection tmp;
-
 	inter.t = DBL_MAX;	 // Make an impossibly large t
 	for (shapes.MoveFirst(); !shapes.AtEnd(); shapes.MoveNext())
 	{
 		shape *s = shapes.GetCurrent();
-		shape curShape = *s; //.
-		found = curShape.Intersect(r, inter); //.
-		
+		shape curShape = *s;
+		inter.v = -1*r.v;
+		found = curShape.CalculateIntersection(r, inter); //CalcInter method can be found in Shape.cpp
 	}
 	return found;
 }
